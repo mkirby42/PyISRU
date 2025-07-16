3654c7c0819bbb728ee93d23da26e959f7e1e1cb
Mkirby42 <mkirby3@angelo.edu>
2025-07-15 20:19:58 -0600

    running

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def run_simulation_route():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
     # app.run(debug=True)
